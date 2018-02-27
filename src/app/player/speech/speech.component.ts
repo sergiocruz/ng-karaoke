46072739db637768a43d0d33de6bf0ab6ac41291
Sergio Cruz <hello@sergiocruz.me>
2018-02-27 12:12:28 -0500

    Adds basic speech recognition

---
+++ src/app/player/speech/speech.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Observable, Subscription, } from 'rxjs'
+import { interval } from 'rxjs/observable/interval';
+import { timer } from 'rxjs/observable/timer';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/distinct'
+import 'rxjs/add/operator/timeout'
+
+import { RecognitionService } from './recognition.service'
+
+@Component({
+  selector: 'player-speech',
+  templateUrl: './speech.component.html',
+  styleUrls: ['./speech.component.css']
+})
+export class SpeechComponent implements OnInit, OnDestroy {
+
+  private subscriptions: Subscription[] = []
+  private recognition: SpeechRecognition
+  public isAutoRestarting: boolean = false
+  public isRecording: boolean = false
+  public text: string
+
+  constructor(
+    private RecognitionService: RecognitionService
+  ) { }
+
+  ngOnInit() {
+    this.recognition = this.RecognitionService.getRecognition()
+    const result$ = fromEvent(this.recognition, 'result')
+    const start$ = fromEvent(this.recognition, 'start')
+    const stop$ = fromEvent(this.recognition, 'stop')
+    const end$ = fromEvent(this.recognition, 'end')
+
+    const onStart = start$.subscribe(() => {
+      console.log('start')
+      this.isRecording = true
+
+      result$.timeout(5000).subscribe(null, () => {
+        if (this.isRecording) {
+          console.log('timeout, restarting...')
+          this.isAutoRestarting = true
+          this.recognition.stop()
+        }
+      })
+    })
+
+    const onEnd = Observable.merge(stop$, end$).subscribe(() => {
+      console.log('stop or end?')
+      if (this.isAutoRestarting) {
+        this.isAutoRestarting = false
+        this.recognition.start()
+      } else {
+        this.isRecording = false
+      }
+    })
+
+    const onResult = result$
+      .map((e: SpeechRecognitionEvent) => e.results[e.results.length - 1])
+      .filter((result: SpeechRecognitionResult) => result.isFinal)
+      .map((result: SpeechRecognitionResult) => result[0].transcript)
+      .distinct()
+      .subscribe((t: string) => this.text = t)
+
+    // So we can unsubscribe later
+    this.subscriptions = this.subscriptions.concat([ onStart, onEnd, onResult ])
+  }
+
+  ngOnDestroy() {
+    this.recognition.stop()
+    this.subscriptions.forEach((s) => s.unsubscribe())
+  }
+
+  handleToggle() {
+    if (!this.isRecording) {
+      this.recognition.start()
+    } else {
+      this.recognition.stop()
+    }
+  }
+
+}
